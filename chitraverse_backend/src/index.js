efc590e949f6bba6c2c5e52d5574a792e822f25b
NirojBhattarai <neerajbhttri@gmail.com>
2024-11-20 20:41:18 +0545

    Establish Database Connection using Mongoose

--- chitraverse_backend/src/index.js
+++ chitraverse_backend/src/index.js
@@ -1,13 +1,18 @@
 import dotenv from 'dotenv';
 import {app} from './app.js';
-
+import connectDB from './db/index.js'
 
 dotenv.config({
     path:"./.env"});
 
 const PORT = process.env.PORT;
 
-
-app.listen(PORT, ()=>{
-    console.log(`Server is running successfully on port:${PORT}`);
+connectDB()
+.then(()=>{
+    app.listen(PORT, ()=>{
+        console.log(`Server is running successfully on port:${PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("MongoDB Connection Error",error);
 })
