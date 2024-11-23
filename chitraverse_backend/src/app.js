221016e044a441ab5925d511464d62b8c69e85ea
NirojBhattarai <neerajbhttri@gmail.com>
2024-11-23 10:02:45 +0545

    Use healthcheck Router in app.js

--- chitraverse_backend/src/app.js
+++ chitraverse_backend/src/app.js
@@ -1,5 +1,6 @@
 import express, { urlencoded } from 'express';
 import cors from 'cors';
+import healthcheckRouter from './routes/healthcheck.routes.js'
 
 const app = express();
 app.use(cors({
@@ -11,6 +12,9 @@ app.use(cors({
 app.use(express.json({limit:"16kb"}));
 app.use(urlencoded({extended:true,limit:"16kb"}));
 app.use(express.static("public"));
+
+//Routes
+app.use("/api/v1/healthcheck",healthcheckRouter)
 
 
 export {app};
