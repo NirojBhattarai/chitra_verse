bef2c3559b068b8fabe3dbdb403f9d49c1263219
NirojBhattarai <neerajbhttri@gmail.com>
2024-11-23 10:17:52 +0545

    Fix issues in import statement on healthcheck Controller

--- chitraverse_backend/src/controllers/healthcheck.controller.js
+++ chitraverse_backend/src/controllers/healthcheck.controller.js
@@ -1,7 +1,7 @@
-import {ayncHandler} from '../utils/asyncHandler';
-import {apiResponse} from '../utils/apiResponse';
+import {asyncHandler} from '../utils/asyncHandler.js';
+import {apiResponse} from '../utils/apiResponse.js';
 
-const healthcheck = ayncHandler(async (req,res)=>{
+const healthcheck = asyncHandler(async (req,res)=>{
     return res
         .status(200)
         .json(new apiResponse(200, "Ok", "Health Check Successful !!"));
