7a3e66f65be8bb02cc5e940300032d1bdccd3c91
NirojBhattarai <neerajbhttri@gmail.com>
2024-11-24 09:19:47 +0545

    Add Function to Compare Plain Text Password with Encrypted Password Stored on Database

--- chitraverse_backend/src/models/user.models.js
+++ chitraverse_backend/src/models/user.models.js
@@ -58,4 +58,9 @@ userSchema.pre("save", async function (next){
     next();
 })
 
+// Comparing Plain Text Password with Encrypted Password
+userSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password, this.password);
+}
+
 export const User = mongoose.model("User",userSchema);
