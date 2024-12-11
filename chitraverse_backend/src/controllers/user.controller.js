23682f601656fa7caca9706b5bc4803eb622e0b0
NirojBhattarai <neerajbhttri@gmail.com>
2024-12-11 15:42:16 +0545

    feat: Add User Data Validation in registerUser Controller

--- chitraverse_backend/src/controllers/user.controller.js
+++ chitraverse_backend/src/controllers/user.controller.js
@@ -1,9 +1,26 @@
 import { asyncHandler } from "../utils/asyncHandler";
+import apiError from "../utils/apiError";
+import { User } from "../models/user.models";
 
-const registerUser = asyncHandler(async(req,res)=>{
+const registerUser = asyncHandler(async (req, res) => {
+    const { fullname, email, username, password } = req.body;
 
+    // User validation
+    if (
+        [fullname, email, username, password].some(
+            (field) => field?.trim() === ""
+        )
+    ) {
+        throw new apiError(400, "All fields are required");
+    }
+
+   const existedUser = await User.findOne({
+        $or: [{fullname},{email}]
+    })
+
+    if(existedUser){
+        throw new apiError(409,"User with email or username already exists");
+    }
 });
 
-export {
-    registerUser
-}
+export { registerUser };
