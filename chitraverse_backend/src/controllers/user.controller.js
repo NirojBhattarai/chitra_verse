26786fc675c9ef5f49012dff4b14ae18f8e9d724
NirojBhattarai <neerajbhttri@gmail.com>
2024-12-11 15:52:50 +0545

    feat: Add File Handling Function to Upload Avatar and Cover Image in registerUser

--- chitraverse_backend/src/controllers/user.controller.js
+++ chitraverse_backend/src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler";
 import apiError from "../utils/apiError";
 import { User } from "../models/user.models";
+import {uploadOnCloudinary} from "../utils/cloudinary"
 
 const registerUser = asyncHandler(async (req, res) => {
     const { fullname, email, username, password } = req.body;
@@ -21,6 +22,16 @@ const registerUser = asyncHandler(async (req, res) => {
     if(existedUser){
         throw new apiError(409,"User with email or username already exists");
     }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    const coverLocalPath = req.files?.coverImage[0]?.path
+
+    if(!avatarLocalPath || !coverLocalPath){
+        throw new apiError(400, "Avatar or Cover Image is missing");
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    const coverImage = await uploadOnCloudinary(coverLocalPath);
 });
 
 export { registerUser };
