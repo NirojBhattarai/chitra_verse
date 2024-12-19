8b6310c7e2a5df6142397be8595ad7bc5c9d55a7
NirojBhattarai <neerajbhttri@gmail.com>
2024-12-19 13:21:15 +0545

    fix: Fix missing JWT import in userSchema

--- chitraverse_backend/src/models/user.models.js
+++ chitraverse_backend/src/models/user.models.js
@@ -1,89 +1,91 @@
 //User Database Schema
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
-    {
-        username:{
-            type:String,
-            required:[true,"Username is required"],
-            unique:true,
-            lowercase:true,
-            trim:true,
-            index:true
-        },
-        email:{
-            type:String,
-            required:[true,"Email is required"],
-            unique:true,
-            trim:true
-        },
-        fullname:{
-            type:String,
-            required:true,
-            trim:true,
-            index:true
-        },
-        avatar:{
-            type:String,
-            required:true,
-        },
-        coverImage:{
-            type:String,
-        },
-       watchHistory:[
-        {
-            type:Schema.Types.ObjectId,
-            ref:"Video"
-        }
-       ],
-       password:{
-            type:String,
-            required:[true,"Password is required"],
-       },
-       refreshToken:{
-            type:String,
-       }
-
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
     },
-    {
-        timestamps:true,
-    }
-)
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 // Encrypting Password Before Saving or Updating on Database
-userSchema.pre("save", async function (next){
-    if(!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password,10);
-    next();
-})
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 // Comparing Plain Text Password with Encrypted Password
-userSchema.methods.isPasswordCorrect = async function (password){
-    return await bcrypt.compare(password, this.password);
-}
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 //Function to Generate Access Token
-userSchema.methods.generateAccessToken = function (){
-    return jwt.sign({
-        _id: this._id,
-        email: this.email,
-        username: this.username,
-        fullname: this.fullname
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
     },
     process.env.ACCESS_TOKEN_SECRET,
-    {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
-    );
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
 };
 
 //Function to Generate Refresh Token
-userSchema.methods.generateRefreshToken = function (){
-    return jwt.sign({
-        _id: this._id,
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
     },
     process.env.REFRESH_TOKEN_SECRET,
-    {expiresIn: process.env.REFRESH_TOKEN_EXPIRY}
-    );
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
 };
 
-export const User = mongoose.model("User",userSchema);
+export const User = mongoose.model("User", userSchema);
