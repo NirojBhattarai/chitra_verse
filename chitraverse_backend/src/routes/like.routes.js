cda941ba8ef6ecf9a6bde786a2928a4b07558a2a
NirojBhattarai <neerajbhttri@gmail.com>
2024-12-30 17:06:12 +0545

    add: Add Function to Fetch All Liked Videos By User

--- chitraverse_backend/src/routes/like.routes.js
+++ chitraverse_backend/src/routes/like.routes.js
@@ -1,11 +1,12 @@
 import {Router} from "express";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
-import { toogleCommentLike, toogleVideoLike, toogleTweetLike } from "../controllers/like.controller.js";
+import { toogleCommentLike, toogleVideoLike, toogleTweetLike,getLikedVideos } from "../controllers/like.controller.js";
 
 const router = Router();
 
 router.route('/likevideo/:id').post(verifyJWT,toogleVideoLike);
 router.route('/likecomment/:id').post(verifyJWT,toogleCommentLike);
 router.route('/liketweet/:id').post(verifyJWT,toogleTweetLike);
+router.route('/viewlikedvideos').post(verifyJWT,getLikedVideos);
 
 export default router;
