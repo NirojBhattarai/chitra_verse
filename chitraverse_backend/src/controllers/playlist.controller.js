ee9e04b0ff77ae55278c8a957af17a28503cfdda
NirojBhattarai <neerajbhttri@gmail.com>
2024-12-31 19:00:10 +0545

    add: Add Function to Get Playlist By User Id in Playlist Controller

--- chitraverse_backend/src/controllers/playlist.controller.js
+++ chitraverse_backend/src/controllers/playlist.controller.js
@@ -36,4 +36,51 @@ const createPlaylist = asyncHandler(async (req, res) => {
   }
 });
 
-export { createPlaylist };
+const getUserPlaylist = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+
+  if (!userId) {
+    throw new apiError(400, "User Invalid or Unauthorized Request");
+  }
+  try {
+    const userplaylist = await Playlist.find({ owner: userId });
+
+    if (!userplaylist) {
+      throw new apiError(400, "Error while fetching playlist");
+    }
+
+    return res
+      .status(200)
+      .json(
+        new apiResponse(200, userplaylist, "User Playlist Fetched Successfully")
+      );
+  } catch (error) {
+    console.log("Error while fetching user playlist", error);
+    throw new apiError(400, "Something went wrong while fetching playlist");
+  }
+});
+
+const getPlaylistById = asyncHandler(async(req, res) =>{
+  const playlistId = req.params;
+
+  if(!playlistId){
+    throw new apiError(400, "Invalid or Missing PlaylistId");
+  }
+
+  try {
+    const playlist = await Playlist.findById(playlistId);
+
+    if(!playlist){
+      throw new apiError(400, "Error while fetching playlist by id");
+    }
+
+    return res
+    .status(200)
+    .json(new apiResponse(200, playlist, "Playlist Fetched Successfully by Id"));
+  } catch (error) {
+    console.log("Error while fetching Playlist By Id");
+    throw new apiError(400, "Something went wrong while fetching Playlist");
+  }
+});
+
+export { createPlaylist, getUserPlaylist, getPlaylistById };
--- chitraverse_backend/src/routes/playlist.routes.js
+++ chitraverse_backend/src/routes/playlist.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
-import { createPlaylist } from "../controllers/playlist.controller.js";
+import { createPlaylist, getUserPlaylist } from "../controllers/playlist.controller.js";
 
 const router = Router();
 
 router.route('/create').post(verifyJWT,createPlaylist);
+router.route('/viewplaylist').post(verifyJWT,getUserPlaylist);
+router.route('/getplaylist/:id').post(verifyJWT,getUserPlaylist);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
 
 export default router;
