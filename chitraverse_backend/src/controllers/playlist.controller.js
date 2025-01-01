d5952f41db91591b5e8404d082505bcfc07569ef
NirojBhattarai <neerajbhttri@gmail.com>
2025-01-01 17:53:08 +0545

    add: Add Function to Add Video to the Playlist in Playlist Controller

--- chitraverse_backend/src/controllers/playlist.controller.js
+++ chitraverse_backend/src/controllers/playlist.controller.js
@@ -60,27 +60,94 @@ const getUserPlaylist = asyncHandler(async (req, res) => {
   }
 });
 
-const getPlaylistById = asyncHandler(async(req, res) =>{
+const getPlaylistById = asyncHandler(async (req, res) => {
   const playlistId = req.params;
 
-  if(!playlistId){
+  if (!playlistId) {
     throw new apiError(400, "Invalid or Missing PlaylistId");
   }
 
   try {
     const playlist = await Playlist.findById(playlistId);
 
-    if(!playlist){
+    if (!playlist) {
       throw new apiError(400, "Error while fetching playlist by id");
     }
 
     return res
-    .status(200)
-    .json(new apiResponse(200, playlist, "Playlist Fetched Successfully by Id"));
+      .status(200)
+      .json(
+        new apiResponse(200, playlist, "Playlist Fetched Successfully by Id")
+      );
   } catch (error) {
     console.log("Error while fetching Playlist By Id");
     throw new apiError(400, "Something went wrong while fetching Playlist");
   }
 });
 
-export { createPlaylist, getUserPlaylist, getPlaylistById };
+const addVideoToPlaylist = asyncHandler(async (req, res) => {
+  const { id: playlistId } = req.params;
+  
+  if (!playlistId) {
+    throw new apiError(400, "Invalid or Missing PlaylistId");
+  }
+
+  const { videoId } = req.body;
+ 
+  if (!videoId) {
+    throw new apiError(400, "Invalid or Missing VideoId");
+  }
+
+  try {
+    const isVideoInPlaylist = await Playlist.findOne({
+      _id: playlistId,
+      videos: { $in: [videoId] },
+    });
+
+    if (isVideoInPlaylist) {
+      return res
+      .status(400)
+      .json(
+        new apiError(
+          400,
+          isVideoInPlaylist,
+          "Video already exists in the playlist"
+        )
+      );
+    } else {
+      const playlist = await Playlist.findByIdAndUpdate(
+        playlistId,
+        {
+          $push: {
+            videos: videoId,
+          },
+        },
+        {
+          new: true,
+        }
+      );
+
+      if (!playlist) {
+        throw new apiError(400, "Error while adding video to the playlist");
+      }
+
+      return res
+        .status(200)
+        .json(
+          new apiResponse(
+            200,
+            playlist,
+            "Video added to the playlist successfully"
+          )
+        );
+    }
+  } catch (error) {
+    console.log("Error while adding video to the playlist");
+    throw new apiError(
+      400,
+      " Something went wrong while adding video to the playlist"
+    );
+  }
+});
+
+export { createPlaylist, getUserPlaylist, getPlaylistById, addVideoToPlaylist };
