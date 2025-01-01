273c1a5f1ffb3619b135d89ae50f122db3fb3b3b
NirojBhattarai <neerajbhttri@gmail.com>
2025-01-01 22:22:40 +0545

    add: Add Route Path for Subscription Controller

--- chitraverse_backend/src/app.js
+++ chitraverse_backend/src/app.js
@@ -6,6 +6,7 @@ import videoRouter from './routes/video.routes.js';
 import tweetRouter from './routes/tweet.routes.js';
 import likeRouter from './routes/like.routes.js';
 import playlistRouter from './routes/playlist.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
 import cookieParser from 'cookie-parser';
 
 const app = express();
@@ -28,6 +29,7 @@ app.use("/api/v1/videos",videoRouter);
 app.use("/api/v1/tweets",tweetRouter);
 app.use("/api/v1/likes",likeRouter);
 app.use("/api/v1/playlists",playlistRouter);
+app.use("/api/v1/subscriptions",subscriptionRouter);
 
 
 export {app};
