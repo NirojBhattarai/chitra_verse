a7ae21b0b4230074d08f1e213e7599e503c9ba3b
NirojBhattarai <neerajbhttri@gmail.com>
2025-01-03 16:56:03 +0545

    add: Implement user registration and login API functions

---
+++ chitraverse_frontend/src/api/auth.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const registerUser = async(userData) => {
+    const response = await axios.post('auth/register', userData);
+    return response.data;
+};
+
+export const loginUser = async(credentials) => {
+    const response = await axios.post('auth/login',credentials);
+    return response.data;
+}
