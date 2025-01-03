d448e9d253d0d9740bc1db882f55763a38f48a2c
NirojBhattarai <neerajbhttri@gmail.com>
2025-01-03 20:40:23 +0545

    add: Implement login route in the main application

--- chitraverse_frontend/src/App.jsx
+++ chitraverse_frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './pages/Auth/Register';
 import { AuthProvider } from './context/AuthContext';
+import Login from './pages/Auth/Login';
 
 const App = () => (
   <AuthProvider>
     <Router>
       <Routes>
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   </AuthProvider>
