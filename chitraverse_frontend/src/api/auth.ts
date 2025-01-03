ab52f2b100e8a74f3d513e849cd8f37d4735baa7
NirojBhattarai <neerajbhttri@gmail.com>
2025-01-03 23:19:54 +0545

    update: Update API Calls to be called With Credentials True in auth.ts

--- chitraverse_frontend/src/api/auth.ts
+++ chitraverse_frontend/src/api/auth.ts
@@ -1,14 +1,34 @@
 import axios from "axios";
 
-export const registerUser = async(userData:FormData) => {
-    const response = await axios.post('http://localhost:5000/api/v1/users/register', userData);
-    return response.data;
+axios.defaults.withCredentials = true; 
+
+export const registerUser = async (userData: FormData) => {
+  const response = await axios.post(
+    "http://localhost:5000/api/v1/users/register",
+    userData
+  );
+  return response.data;
 };
 
-export const loginUser = async(credentials:{
-    email:String,
-    password:String
+export const loginUser = async (credentials: {
+  email: String;
+  password: String;
 }) => {
-    const response = await axios.post('http://localhost:5000/api/v1/users/login',credentials);
-    return response.data;
-}
+  const response = await axios.post(
+    "http://localhost:5000/api/v1/users/login",
+    credentials
+  );
+  return response.data;
+};
+
+export const fetchAuthenticatedUser = async () => {
+  const response = await axios.post("http://localhost:5000/api/v1/users/viewuser");
+  return response.data;
+};
+
+export const refreshAccessToken = async () => {
+  const response = await axios.post(
+    "http://localhost:5000/api/v1/users/refreshToken"
+  );
+  return response.data;
+};
