195932622eb743f61dac04d788a6602b05234423
NirojBhattarai <neerajbhttri@gmail.com>
2025-01-06 21:19:49 +0545

    feat: Add PlaylistProvider to App Component for Playlist Context Management

--- chitraverse_frontend/src/App.tsx
+++ chitraverse_frontend/src/App.tsx
@@ -2,12 +2,15 @@ import { RouterProvider } from "react-router";
 import router from "./router/routes";
 import { AuthProvider } from "./context/AuthContext";
 import { VideoProvider } from "./context/VideoContext";
+import { PlaylistProvider } from "./context/PlaylistContext";
 function App() {
   return (
     <>
       <AuthProvider>
         <VideoProvider>
+          <PlaylistProvider>
         <RouterProvider router={router} />
+        </PlaylistProvider>
         </VideoProvider>
       </AuthProvider>
       
