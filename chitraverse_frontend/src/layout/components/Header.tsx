7a4ca370d926f4040988e029a51438c76dc6abf9
NirojBhattarai <neerajbhttri@gmail.com>
2025-01-06 15:48:08 +0545

    fix: Adjust padding in Header component's search input for improved usability

--- chitraverse_frontend/src/layout/components/Header.tsx
+++ chitraverse_frontend/src/layout/components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
             <div className="relative">
               <input
                 type="text"
-                className="w-full pl-10 pr-4 py-2 rounded-lg bg-white text-gray-800 focus:ring-2 focus:ring-purple-300 focus:outline-none"
+                className="w-full pl-8 pr-4 py-2 rounded-lg bg-white text-gray-800 focus:ring-2 focus:ring-purple-300 focus:outline-none"
                 placeholder="Search..."
               />
               <FaSearch className="absolute left-3 top-2.5 text-red-600" />
@@ -33,7 +33,7 @@ const Header = () => {
           <div className="relative w-full">
             <input
               type="text"
-              className="w-full pl-10 pr-4 py-2 rounded-lg bg-white text-gray-800 focus:ring-2 focus:ring-purple-300 focus:outline-none"
+              className="w-full pl-8 pr-4 py-2 rounded-lg bg-white text-gray-800 focus:ring-2 focus:ring-purple-300 focus:outline-none"
               placeholder="Search..."
             />
             <FaSearch className="absolute left-3 top-2.5 text-red-600" />
