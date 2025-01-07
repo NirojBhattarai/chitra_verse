dfe08046f5c719c3da33bac69d0c04923454fc54
NirojBhattarai <neerajbhttri@gmail.com>
2025-01-07 19:39:30 +0545

    feat: Add fetchVideosById Function for Fetching Video Details by ID

--- chitraverse_frontend/src/api/video.ts
+++ chitraverse_frontend/src/api/video.ts
@@ -5,3 +5,12 @@ export const fetchVideos = async () => {
     const response = await axios.post('http://localhost:5000/api/v1/videos/view'); 
     return response.data.response.data; 
   };
+
+  export const fetchVideosById = async (_id:string) => {
+    const response = await axios.post(`http://localhost:5000/api/v1/videos/viewvideo`,
+      _id
+    );
+    console.log(response.data);
+    return response.data;
+    
+  }
