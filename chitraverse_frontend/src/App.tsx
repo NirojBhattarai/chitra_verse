6eefc6a4b84d9b864317120108a8622905273b21
NirojBhattarai <neerajbhttri@gmail.com>
2025-01-08 22:15:16 +0545

    feat: Integrate LikedVideosProvider and add Route for LikedVideos page

--- chitraverse_frontend/src/App.tsx
+++ chitraverse_frontend/src/App.tsx
@@ -3,17 +3,19 @@ import router from "./router/routes";
 import { AuthProvider } from "./context/AuthContext";
 import { VideoProvider } from "./context/VideoContext";
 import { PlaylistProvider } from "./context/PlaylistContext";
+import { LikedVideosProvider } from "./context/LikeContext";
 function App() {
   return (
     <>
       <AuthProvider>
         <VideoProvider>
           <PlaylistProvider>
-        <RouterProvider router={router} />
-        </PlaylistProvider>
+            <LikedVideosProvider>
+              <RouterProvider router={router} />
+            </LikedVideosProvider>
+          </PlaylistProvider>
         </VideoProvider>
       </AuthProvider>
-      
     </>
   );
 }
