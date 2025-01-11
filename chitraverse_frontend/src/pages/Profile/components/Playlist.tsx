f7d90cbc67edaa13f7c71acc9bbb04e3be920dca
NirojBhattarai <neerajbhttri@gmail.com>
2025-01-11 22:29:30 +0545

    fix: Adjust Padding in Playlist Component for Improved Layout

--- chitraverse_frontend/src/pages/Profile/components/Playlist.tsx
+++ chitraverse_frontend/src/pages/Profile/components/Playlist.tsx
@@ -21,7 +21,7 @@ const Playlist = () => {
   }
 
   return (
-    <div className="container mx-auto px-4 py-12">
+    <div className="container mx-auto px-4 py-4">
       <h1 className="text-3xl font-bold text-gray-800 mb-6">Your Playlists</h1>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
